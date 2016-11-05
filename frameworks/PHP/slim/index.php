c4eaee6a4ff262d0fcd227a5fd25b39822bd90cd
herloct <herloct@gmail.com>
2016-11-05 22:03:10 +0700

    Use `[]` instead of `array()`.

--- frameworks/PHP/slim/index.php
+++ frameworks/PHP/slim/index.php
@@ -3,7 +3,7 @@ error_reporting(-1);
 
 require_once __DIR__.'/vendor/autoload.php';
 
-$app = new Slim\App(array(
+$app = new Slim\App([
     'db' => function ($c) {
         $pdo = new PDO(
             'mysql:host=localhost;dbname=hello_world;charset=utf8', 
@@ -21,7 +21,7 @@ $app = new Slim\App(array(
     'view' => function ($c) {
         return new Slim\Views\PhpRenderer("templates/");
     }
-));
+]);
 
 // Test 1: Plaintext
 $app->get('/plaintext', function ($request, $response) {
@@ -34,7 +34,7 @@ $app->get('/plaintext', function ($request, $response) {
 // Test 2: JSON serialization
 $app->get('/json', function ($request, $response) {
     return $response
-        ->withJson(array('message' => 'Hello, World!'))
+        ->withJson(['message' => 'Hello, World!'])
         ->withHeader('Content-Type', 'application/json') // fixes utf-8 warning
         ;
 });
@@ -42,7 +42,7 @@ $app->get('/json', function ($request, $response) {
 // Test 3: Single database query
 $app->get('/db', function ($request, $response) {
     $sth = $this->db->prepare('SELECT * FROM World WHERE id = ?');
-    $sth->execute(array(mt_rand(1, 10000)));
+    $sth->execute([mt_rand(1, 10000)]);
     $world = $sth->fetch();
     # Cast fields to int so they don't get wrapped with quotes
     $world['id'] = (int) $world['id'];
@@ -59,9 +59,9 @@ $app->get('/dbs', function ($request, $response) {
     $queries = max(1, min($request->getParam('queries'), 500));
 
     $sth = $this->db->prepare('SELECT * FROM World WHERE id = ?');
-    $worlds = array();
+    $worlds = [];
     for ($i = 0; $i < $queries; ++$i) {
-        $sth->execute(array(mt_rand(1, 10000)));
+        $sth->execute([mt_rand(1, 10000)]);
         $world = $sth->fetch();
         # Cast fields to int so they don't get wrapped with quotes
         $world['id'] = (int) $world['id'];
@@ -82,17 +82,17 @@ $app->get('/updates', function ($request, $response) {
     $sth = $this->db->prepare('SELECT * FROM World WHERE id = ?');
     $updateSth = $this->db->prepare('UPDATE World SET randomNumber = ? WHERE id = ?');
 
-    $worlds = array();
+    $worlds = [];
     for ($i = 0; $i < $queries; ++$i) {
         $id = mt_rand(1, 10000);
         $random_number = mt_rand(1, 10000);
-        $sth->execute(array($id));
+        $sth->execute([$id]);
         $world = $sth->fetch();
         # Cast fields to int so they don't get wrapped with quotes
         $world['id'] = (int) $world['id'];
         $world['randomNumber'] = $random_number;
 
-        $updateSth->execute(array($world['randomNumber'], $world['id']));
+        $updateSth->execute([$world['randomNumber'], $world['id']]);
 
         $worlds[] = $world;
     }
@@ -109,7 +109,10 @@ $app->get('/fortunes', function ($request, $response) {
     $sth->execute();
     $fortunes = $sth->fetchAll();
 
-    array_push($fortunes, array('id'=> 0, 'message' => 'Additional fortune added at request time.'));
+    array_push($fortunes, [
+        'id'=> 0, 
+        'message' => 'Additional fortune added at request time.'
+    ]);
     usort($fortunes, function($left, $right) {
         return strcmp($left['message'], $right['message']);
     });
